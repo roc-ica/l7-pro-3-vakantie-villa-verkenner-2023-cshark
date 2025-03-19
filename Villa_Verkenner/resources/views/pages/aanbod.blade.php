788109036acdca2103c182c65e988c0bc1db9292
Roan Gerlings <r.gerlings75@gmail.com>
2025-03-19 08:53:51 +0100

    small UX change

--- Villa_Verkenner/resources/views/pages/aanbod.blade.php
+++ Villa_Verkenner/resources/views/pages/aanbod.blade.php
@@ -65,32 +65,36 @@
 
     <!-- House Cards -->
     <div class="card-container">
-      @foreach ($houses as $house)
-        <div class="house">
-          <div class="price">
-            <img src="{{ asset('images/verf/verf_lichtpaars.webp') }}" alt="image">
-            <span>€{{ number_format($house->price, 0, ',', '.') }}</span>
+      @if ($houses->isEmpty())
+        <p>Geen huizen gevonden.</p>
+      @else
+        @foreach ($houses as $house)
+          <div class="house">
+            <div class="price">
+              <img src="{{ asset('images/verf/verf_lichtpaars.webp') }}" alt="image">
+              <span>€{{ number_format($house->price, 0, ',', '.') }}</span>
+            </div>
+            {{-- Use a placeholder image for the house --}}
+            <img src="https://picsum.photos/600/400" alt="Lorem Picsum House Image">
+            <h1 class="house-title">{{ $house->name }}</h1>
+            <p class="house-info">{{ $house->address }}</p>
+            <a class="see-button" href="{{ route('detail', $house->id) }}">
+              <img src="{{ asset('images/verf/donker-groen.png') }}" alt="image">
+              <span>Bekijk</span>
+            </a>
+            <br>
+            <p class="house-info">
+              {{ $house->rooms }} slaapkamers | 
+              @if($house->geoOptions->isNotEmpty())
+                {{ $house->geoOptions->pluck('name')->implode(', ') }}
+              @endif
+              @if($house->features->isNotEmpty())
+                | Features: {{ $house->features->pluck('name')->implode(', ') }}
+              @endif
+            </p>
           </div>
-          {{-- Use a placeholder image for the house --}}
-          <img src="https://picsum.photos/600/400" alt="Lorem Picsum House Image">
-          <h1 class="house-title">{{ $house->name }}</h1>
-          <p class="house-info">{{ $house->address }}</p>
-          <a class="see-button" href="{{ route('detail', $house->id) }}">
-            <img src="{{ asset('images/verf/donker-groen.png') }}" alt="image">
-            <span>Bekijk</span>
-          </a>
-          <br>
-          <p class="house-info">
-            {{ $house->rooms }} slaapkamers | 
-            @if($house->geoOptions->isNotEmpty())
-              {{ $house->geoOptions->pluck('name')->implode(', ') }}
-            @endif
-            @if($house->features->isNotEmpty())
-              | Features: {{ $house->features->pluck('name')->implode(', ') }}
-            @endif
-          </p>
-        </div>
-      @endforeach
+        @endforeach
+      @endif
     </div>
   </main>
 </x-layout>
